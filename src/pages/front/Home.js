7ddc8b853edeea2bf4b48fda58b2217b9c9b2816
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2021-08-11 03:49:17 +0300

    small issue

--- src/pages/front/Home.js
+++ src/pages/front/Home.js
@@ -5,68 +5,64 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-           {/* -------------------------------------------------------------------- */}
+          
 
+            <div class="container">
+                <div class="row" style={{ marginRight: "50%" }}>
+                    <div class=" about-content animated fadeIn scrollable" >
+                        <h1 class="animated fadeIn one">Thank You</h1>
+                        <h3 class="animated fadeIn one mt-4">About us</h3>
+                        <div class="about-me" style={{ transform: "translateY(0px) translateZ(0px)", marginTop: "0px" }}>
+                            <div class="animated fadeIn two">
+                                <p>Basma is redefining oral health by using modern technology to help deliver human-focused care. With a commitment to creating the best outcomes, Basma inspires confidence and promotes wellbeing.
 
-
-
-
-           <div class="container">
-    	<div class="row" style={{marginRight:"50%"}}>
-    	    <div class=" about-content animated fadeIn scrollable" >
-    	    	<h1 class="animated fadeIn one">Thank You</h1>
-    	    	<h3 class="animated fadeIn one mt-4">About us</h3>
-    	       	<div class="about-me" style={{transform: "translateY(0px) translateZ(0px)", marginTop:"0px"}}>
-    	       		<div class="animated fadeIn two">
-    	            	<p>Basma is redefining oral health by using modern technology to help deliver human-focused care. With a commitment to creating the best outcomes, Basma inspires confidence and promotes wellbeing.
-
-Customers order and complete an at-home impression kit, send it back to us, and we deliver their custom invisible aligners right to them. 
+                                Customers order and complete an at-home impression kit, send it back to us, and we deliver their custom invisible aligners right to them.
 
 We are cutting down the costs of getting orthodontic treatment by up to 65% and giving our customers the perfect smile in an average of 6 months. Our board-certified orthodontists oversee the entire treatment process, from designing to manufacturing, so our customers are always in good hands and feel connected constantly to their doctors on our platform.</p>
-    	            	<p>Invisible Aligners, Teeth Straightening, Health/Beauty, Cosmetic Dentistry, Health, and Digital Health</p>
-    	            </div>
-    	       		<div class="Synopsis-highlightLine five" style={{transform: "matrix(1, 0, 0, 1, 0, 0)"}}></div>
-    	       		<div class="animated fadeIn three">
-    	            	<p class="Synopsis-highlight">Using the latest technologies, we connect patients to quality dental care from the comfort of home. To make sure our treatment is right for you, one of our specialists will remotely diagnose your case. An orthodontist will then design your treatment and  monitor your progress through our platform, from start to finish.</p>
-    	            </div>
-    	            <div class="Synopsis-highlightLine five" style={{ transform:" matrix(1, 0, 0, 1, 0, 0)"}}></div>
-    	            <div class="animated fadeIn four">
-    	            	<p>BASMA helps customers get a healthier smile, full of confidence. Watch their stories right here.</p>
-    		            
-    	            </div>
-    	        </div>
-    	    </div>
-          <div class="about-panel-wrapper">
-      		<div class=" about-panel animated fadeIn">
-  	      	<div class="perspective-container">
-  		        <div class="zero photo-about animated fadeIn" style={{transform:"translateY(0px) translateZ(0px)"}}>
-  		        	<div class="hide-photo-filter animated fadeIn"></div>
-  		        	<div class="about-info">
-  		        		<h3 class="about-info-name animated fadeInUp up-one">BASMA.COM  </h3>
-  		        		<div cla style={{color:"black"}} ss="about-info-contact">
-  		        			<h4 class="animated fadeInUp up-two"> Invisible aligners.
-The better alternative to braces</h4>
-  		        			<p class="animated fadeInUp up-three">London & Beirut</p>
-  		        		
-  		        		</div >
-                          <div style={{marginTop:'35%'}}>
-                          <ul className="social" >
-                            <li><a href="https://www.facebook.com/wearebasma" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                            <li><a href="https://www.linkedin.com/company/basma/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                            <li><a href="http://go.basma.com/home" target="_blank"><i class="fa fa-ravelry" aria-hidden="true"></i></a></li>
-                            </ul>
+                                <p>Invisible Aligners, Teeth Straightening, Health/Beauty, Cosmetic Dentistry, Health, and Digital Health</p>
                             </div>
-  		        	</div>
-  		        </div>
-  	      	</div>
-      	 </div>
-         </div>
-    	</div>
-    </div>
-           {/* -------------------------------------------------------------------------- */}
+                            <div class="Synopsis-highlightLine five" style={{ transform: "matrix(1, 0, 0, 1, 0, 0)" }}></div>
+                            <div class="animated fadeIn three">
+                                <p class="Synopsis-highlight">Using the latest technologies, we connect patients to quality dental care from the comfort of home. To make sure our treatment is right for you, one of our specialists will remotely diagnose your case. An orthodontist will then design your treatment and  monitor your progress through our platform, from start to finish.</p>
+                            </div>
+                            <div class="Synopsis-highlightLine five" style={{ transform: " matrix(1, 0, 0, 1, 0, 0)" }}></div>
+                            <div class="animated fadeIn four">
+                                <p>BASMA helps customers get a healthier smile, full of confidence. Watch their stories right here.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-panel-wrapper">
+                        <div class=" about-panel animated fadeIn">
+                            <div class="perspective-container">
+                                <div class="zero photo-about animated fadeIn" style={{ transform: "translateY(0px) translateZ(0px)" }}>
+                                    <div class="hide-photo-filter animated fadeIn"></div>
+                                    <div class="about-info">
+                                        <h3 class="about-info-name animated fadeInUp up-one">BASMA.COM  </h3>
+                                        <div cla style={{ color: "black" }} ss="about-info-contact">
+                                            <h4 class="animated fadeInUp up-two"> Invisible aligners.
+The better alternative to braces</h4>
+                                            <p class="animated fadeInUp up-three">London & Beirut</p>
+
+                                        </div >
+                                        <div style={{ marginTop: '35%' }}>
+                                            <ul className="social" >
+                                                <li><a href="https://www.facebook.com/wearebasma" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                                <li><a href="https://www.linkedin.com/company/basma/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                                <li><a href="http://go.basma.com/home" target="_blank"><i class="fa fa-ravelry" aria-hidden="true"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
         </div>
     )
 
-    
+
 }
 
